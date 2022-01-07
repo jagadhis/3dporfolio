3def902144535e9a290e1536a469362c4f192f3e
jagadhis_0220 <jwaran78@gmail.com>
2022-01-07 10:36:14 +0530

    commit

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
@@ -30,11 +31,27 @@ const gridHelper = new THREE.GridHelper(200,500);
 scene.add(lightHelper,gridHelper);
 scene.add(pointLight);
 
+const controls = new OrbitControls(camera,renderer.domElement);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25,24,24);
+  const material = new THREE.MeshStandardMaterial({color:0xffffff});
+  const star = new THREE.Mesh(geometry,material);
+
+  const [x,y,z] = Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x,y,z);
+  scene.add(star)
+}
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('image2.jfif');
+scene.background = spaceTexture;
 function animate(){
   requestAnimationFrame(animate);
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+  controls.update();
   renderer.render(scene,camera);
 }
 
